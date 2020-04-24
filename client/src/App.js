3cfc0128065593e54fbecf38d8f140d4744c1948
ricky691 <56139697+ricky691@users.noreply.github.com>
2020-04-23 21:59:16 -0400

    finish

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 import SavedList from './Movies/SavedList';
 
 const App = () => {
-  const [savedList, setSavedList] = useState([]);
-  const [movieList, setMovieList] = useState([]);
-
-  useEffect(() => {
-    const getMovies = () => {
-      axios
-        .get('http://localhost:5000/api/movies')
-        .then(response => {
-          setMovieList(response.data);
-        })
-        .catch(error => {
-          console.error('Server Error', error);
-        });
-    }
-    getMovies();
-  }, []);
+  const [savedList, setSavedList] = useState( [] );
 
   const addToSavedList = movie => {
-    setSavedList([...savedList, movie]);
+    setSavedList( [...savedList, movie] );
   };
 
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Switch>
+        <Route exact path='/' render={(props) => <MovieList {...props} />} />
+        <Route exact path='/movies/:id' render={(props) => <Movie {...props} />} />
+      </Switch>
     </div>
   );
 };
